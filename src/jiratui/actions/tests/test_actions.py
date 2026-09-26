00c9941841d4b1cd73e13a5ceb90b1d12cf999cf
GT <52700315+whyisdifficult@users.noreply.github.com>
2026-09-26 13:07:58 +0200

    JT-84: improve config screen

--- src/jiratui/actions/tests/test_actions.py
+++ src/jiratui/actions/tests/test_actions.py
@@ -65,7 +65,7 @@ from jiratui.widgets.related_work_items.related_issues import (
 )
 from jiratui.widgets.remote_links.links import IssueRemoteLinkCollapsible, IssueRemoteLinksWidget
 from jiratui.widgets.screen import MainScreen, WorkItemSearchResult
-from jiratui.widgets.screens.config import ConfigFileScreen
+from jiratui.widgets.screens.config import ConfigFileScreen, ConfigTable
 from jiratui.widgets.screens.confirmation import ConfirmationScreen
 from jiratui.widgets.screens.git import GitScreen
 from jiratui.widgets.screens.goto import GoToScreen
@@ -6405,6 +6405,146 @@ def test_info_tabbed_content_actions_and_bindings(bindings: dict):
             key_display=None,
             priority=False,
             tooltip='Focus previous tab',
+            id=None,
+            system=False,
+            group=None,
+        ),
+    ]
+
+
+def test_config_table_actions_and_bindings(bindings: dict):
+    assert ConfigTable.ACTIONS == [
+        UIAction(
+            action=SupportedActions.SELECT_CURSOR.value,
+            keys=bindings.get(SupportedActions.SELECT_CURSOR.value, {}).get('keys', []),
+            description='Select the item under the cursor',
+            show=False,
+            tooltip='Select the item under the cursor',
+        ),
+        UIAction(
+            action=SupportedActions.CURSOR_UP.value,
+            keys=bindings.get(SupportedActions.CURSOR_UP.value, {}).get('keys', []),
+            description='Move up',
+            show=False,
+            tooltip='Move up',
+        ),
+        UIAction(
+            action=SupportedActions.CURSOR_DOWN.value,
+            keys=bindings.get(SupportedActions.CURSOR_DOWN.value, {}).get('keys', []),
+            description='Move down',
+            show=False,
+            tooltip='Move down',
+        ),
+        UIAction(
+            action=SupportedActions.PAGE_UP.value,
+            keys=bindings.get(SupportedActions.PAGE_UP.value, {}).get('keys', []),
+            description='Move 1 page up',
+            show=False,
+            tooltip='Move 1 page up',
+        ),
+        UIAction(
+            action=SupportedActions.PAGE_DOWN.value,
+            keys=bindings.get(SupportedActions.PAGE_DOWN.value, {}).get('keys', []),
+            description='Move 1 page down',
+            show=False,
+            tooltip='Move 1 page down',
+        ),
+        UIAction(
+            action=SupportedActions.SCROLL_TOP.value,
+            keys=bindings.get(SupportedActions.SCROLL_TOP.value, {}).get('keys', []),
+            description='Scroll to the top',
+            show=False,
+            tooltip='Scroll to the top',
+        ),
+        UIAction(
+            action=SupportedActions.SCROLL_BOTTOM.value,
+            keys=bindings.get(SupportedActions.SCROLL_BOTTOM.value, {}).get('keys', []),
+            description='Scroll to the bottom',
+            show=False,
+            tooltip='Scroll to the bottom',
+        ),
+    ]
+    assert ConfigTable.BINDINGS == [
+        Binding(
+            key=','.join(bindings.get(SupportedActions.SELECT_CURSOR.value, {}).get('keys', [])),
+            action=SupportedActions.SELECT_CURSOR.value,
+            description='Select the item under the cursor',
+            show=False,
+            key_display=None,
+            priority=False,
+            tooltip='Select the item under the cursor',
+            id=None,
+            system=False,
+            group=None,
+        ),
+        Binding(
+            key=','.join(bindings.get(SupportedActions.CURSOR_UP.value, {}).get('keys', [])),
+            action=SupportedActions.CURSOR_UP.value,
+            description='Move up',
+            show=False,
+            key_display=None,
+            priority=False,
+            tooltip='Move up',
+            id=None,
+            system=False,
+            group=None,
+        ),
+        Binding(
+            key=','.join(bindings.get(SupportedActions.CURSOR_DOWN.value, {}).get('keys', [])),
+            action=SupportedActions.CURSOR_DOWN.value,
+            description='Move down',
+            show=False,
+            key_display=None,
+            priority=False,
+            tooltip='Move down',
+            id=None,
+            system=False,
+            group=None,
+        ),
+        Binding(
+            key=','.join(bindings.get(SupportedActions.PAGE_UP.value, {}).get('keys', [])),
+            action=SupportedActions.PAGE_UP.value,
+            description='Move 1 page up',
+            show=False,
+            key_display=None,
+            priority=False,
+            tooltip='Move 1 page up',
+            id=None,
+            system=False,
+            group=None,
+        ),
+        Binding(
+            key=','.join(bindings.get(SupportedActions.PAGE_DOWN.value, {}).get('keys', [])),
+            action=SupportedActions.PAGE_DOWN.value,
+            description='Move 1 page down',
+            show=False,
+            key_display=None,
+            priority=False,
+            tooltip='Move 1 page down',
+            id=None,
+            system=False,
+            group=None,
+        ),
+        Binding(
+            key=','.join(bindings.get(SupportedActions.SCROLL_TOP.value, {}).get('keys', [])),
+            action=SupportedActions.SCROLL_TOP.value,
+            description='Scroll to the top',
+            show=False,
+            key_display=None,
+            priority=False,
+            tooltip='Scroll to the top',
+            id=None,
+            system=False,
+            group=None,
+        ),
+        Binding(
+            action=SupportedActions.SCROLL_BOTTOM.value,
+            key=','.join(bindings.get(SupportedActions.SCROLL_BOTTOM.value, {}).get('keys', [])),
+            description='Scroll to the bottom',
+            show=False,
+            key_display=None,
+            priority=False,
+            tooltip='Scroll to the bottom',
             id=None,
             system=False,
             group=None,
--- src/jiratui/widgets/screens/config.py
+++ src/jiratui/widgets/screens/config.py
@@ -1,9 +1,10 @@
 import json
 
 from rich.text import Text
+from textual import on
 from textual.app import ComposeResult
 from textual.binding import Binding
-from textual.containers import Vertical
+from textual.containers import Horizontal, VerticalScroll
 from textual.screen import ModalScreen
 from textual.widgets import DataTable, TextArea
 
@@ -53,81 +54,76 @@ class ConfigTable(Actionable, DataTable, inherit_bindings=False):  # type:ignore
 class ConfigFileScreen(ModalScreen):
     """The screen that displays the configuration settings."""
 
-    BINDINGS = [('escape', 'app.pop_screen', 'Close')]
+    BINDINGS = [('escape', 'process_escape', 'Close')]
     TITLE = 'JiraTUI Configuration'
 
     @property
     def datatable_config_info(self) -> ConfigTable:
         return self.query_one('#config-details', expect_type=ConfigTable)
 
+    @property
+    def left_vertical_container(self) -> VerticalScroll:
+        return self.query_one('#left-vertical-container', expect_type=VerticalScroll)
+
+    @property
+    def textarea(self) -> TextArea:
+        return self.query_one(TextArea)
+
     def compose(self) -> ComposeResult:
-        with Vertical() as vertical:
-            vertical.border_title = self.TITLE
+        with Horizontal() as h:
+            h.border_title = self.TITLE
             yield ConfigTable(cursor_type='row', show_header=False, id='config-details')
-            if CONFIGURATION.get().pre_defined_jql_expressions:
-                jql_expressions_textarea = TextArea.code_editor(
-                    language='json',
-                    read_only=True,
-                    classes='config-file-textarea',
-                    show_line_numbers=False,
-                    compact=True,
+            with VerticalScroll(id='left-vertical-container', can_focus=False) as vs:
+                vs.display = False
+                yield TextArea(
+                    language='json', compact=True, read_only=True, classes='config-file-textarea'
                 )
-                jql_expressions_textarea.border_title = 'pre_defined_jql_expressions'
-                yield jql_expressions_textarea
 
     @staticmethod
-    def _get_data():
-        return CONFIGURATION.get().model_dump(
-            exclude={'pre_defined_jql_expressions', 'jira_api_token', 'git_repositories'}
-        )
+    def _get_config_data():
+        return CONFIGURATION.get().model_dump(exclude={'jira_api_token'})
 
-    async def on_mount(self) -> None:
+    @property
+    def _json_fields(self) -> list[str]:
+        return ['pre_defined_jql_expressions', 'styling', 'git_repositories', 'ssl']
+
+    @on(DataTable.RowSelected)
+    def _toggle_left_vertical_container(self, event: DataTable.RowSelected) -> None:
+        left_vertical_container = self.left_vertical_container
+        if not left_vertical_container.display:
+            data = self._get_config_data()
+            if event.row_key.value in self._json_fields and event.row_key.value in data:
+                left_vertical_container.display = True
+                if data.get(event.row_key.value) is not None:
+                    self.textarea.load_text(
+                        json.dumps(data.get(event.row_key.value), indent=3, sort_keys=True)
+                    )
+                    self.textarea.border_title = event.row_key.value
+        else:
+            left_vertical_container.display = False
+
+    def action_process_escape(self) -> None:
+        if self.focused == self.textarea:
+            self.set_focus(self.datatable_config_info)
+            self.left_vertical_container.display = False
+        else:
+            self.dismiss()
+
+    def on_mount(self) -> None:
         table = self.datatable_config_info
         table.add_columns(*['Property', 'Value'])
-        rows = []
-        data = self._get_data()
+        data = self._get_config_data()
         for key, value in data.items():
-            rows.append(
-                (
-                    Text(key, justify='right', style='yellow'),
-                    Text(str(value), justify='left'),
+            if key in self._json_fields:
+                display_value = Text('Press enter to view', justify='left', style='blue')
+            else:
+                row_style = ''
+                if isinstance(value, bool):
+                    row_style = 'green'
+                elif isinstance(value, int):
+                    row_style = 'blue'
+                display_value = Text(
+                    str(value) if value is not None else '-', justify='left', style=row_style
                 )
-            )
 
-        if ssl_config := CONFIGURATION.get().ssl:
-            rows.append(
-                (
-                    Text('ssl.verify_ssl', justify='right', style='yellow'),
-                    Text(str(ssl_config.verify_ssl), justify='left'),
-                )
-            )
-            rows.append(
-                (
-                    Text('ssl.ca_bundle', justify='right', style='yellow'),
-                    Text(str(ssl_config.ca_bundle), justify='left'),
-                )
-            )
-            rows.append(
-                (
-                    Text('ssl.certificate_file', justify='right', style='yellow'),
-                    Text(str(ssl_config.certificate_file), justify='left'),
-                )
-            )
-            rows.append(
-                (
-                    Text('ssl.key_file', justify='right', style='yellow'),
-                    Text(str(ssl_config.key_file), justify='left'),
-                )
-            )
-            rows.append(
-                (
-                    Text('ssl.password', justify='right', style='yellow'),
-                    Text(str(ssl_config.password), justify='left'),
-                )
-            )
-
-        if CONFIGURATION.get().pre_defined_jql_expressions:
-            ta = self.query_one(TextArea)
-            ta.text = json.dumps(CONFIGURATION.get().pre_defined_jql_expressions, indent=3)
-
-        table.add_rows(rows)
+            table.add_row(Text(key, justify='left', style='yellow'), display_value, key=str(key))
